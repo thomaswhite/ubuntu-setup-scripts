992bfcf88038dd78e330ca6ce34a4867329bb7d1
Thomas <thomas.0007@gmail.com>
2018-02-26 14:31:17 +0000

    Refactor setup - move all function loading in the main _setup.sh

--- _setup.sh
+++ _setup.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 download_path="$HOME/tmp"
 download_deb="$download_path/deb"
+build_path="$download_path/buld"
 
-# TODO: Revert commented lines
-# pushd "$download_path"
-#    wget -q -N https://raw.githubusercontent.com/thomaswhite/ubuntu-bash-files/master/.bash_functions_ppa-deb
-    source ./2copy/home/.bash_functions_ppa-deb
-#    wget -q -N https://raw.githubusercontent.com/thomaswhite/ubuntu-bash-files/master/.bash_functions_git
-    source ./2copy/home/.bash_functions_git
-# popd
+mkdir -p $download_deb
+mkdir -p $build_path
 
+pushd $download_path
 
-echo =========================== " $(date +%Y-%m-%d:%H:%M:%S) ${0##*/} "
+if  [  ! declare -F git_clone_or_checkout  ]; then
+  wget -nv -N https://raw.githubusercontent.com/thomaswhite/ubuntu-bash-files/master/.bash_functions_git
+  source .bash_functions_git
+fi
+
+if  [  ! declare -F ppa_add  ]; then
+  wget -nv -N https://raw.githubusercontent.com/thomaswhite/ubuntu-bash-files/master/.bash_functions_ppa-deb
+  source .bash_functions_git
+fi
+
+popd
+echo =========================== " $(date +%Y-%m-%d:%H:%M:%S) ${0##*/} " ===================================
