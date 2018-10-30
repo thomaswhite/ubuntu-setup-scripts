2926a545f2d1d0b72ab7330ff8c026027091c795
Thomas White <thomas.0007@gmail.com>
2018-10-30 15:50:09 +0000

    resilio-sync use mode

--- 05.install-essentials.sh
+++ 05.install-essentials.sh
@@ -37,6 +37,7 @@ sudo apt-fast -qq -y install insync
 sudo apt-fast -qq -y install resilio-sync
 sudo systemctl enable resilio-sync
 sudo sed -i "s/WantedBy=multi-user.target/WantedBy=default.target/g" '/usr/lib/systemd/user/resilio-sync.service '
+sudo systemctl --user enable resilio-sync
 sudo systemctl --user start resilio-sync
 
 sudo apt-fast -y -qq install calibre fbreader
