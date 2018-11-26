f39a8fd22278dac8b19f53c969d545581cdad12a
Thomas <thomas.0007@gmail.com>
2018-11-26 14:56:28 +0000

    Update the list of snap application

--- 011.install-snap-apps.sh
+++ 011.install-snap-apps.sh
@@ -6,15 +6,62 @@ sudo snap install bundler-mardy  cmvs-mardy colmap-mardy  line3dpp-mardy meshlab
 sudo snap install cloudcompare gifcurry
 
 # utilites
-sudo snap install  simplescreenrecorder-mardy tpad termius-app
+sudo snap install  simplescreenrecorder-mardy tpad
 snap install --classic rclone
 
 # audio
-sudo snap install gnome-easytag spotify
+# sudo snap install gnome-easytag
+sudo snap install spotify
+sudo snap install audiobookconverter --edge
+# sudo snap install auryo # soundcloud
 
+# video
+sudo snap install mpv --edge
+sudo snap install vidcutter
+sudo snap install obs-studio
+sudo snap install vlc --edge
+sudo snap install ffmpeg
+sudo snap install shotcut --classic
+sudo snap install matroska-tools
+sudo snap install gifcurry
+sudo snap install animationmaker
+
+
+# 3d modeling
 sudo snap install vectr
 sudo snap install blender --clasic
 
+# vector graphic
+sudo snap install inkscape
+
+# photo
+# sudo snap install gimp
+# sudo snap install darktable
+sudo snap install pencilsheep
+sudo snap install polarr
+sudo snap install xnsketch
+sudo snap install imagenes  # google photos
+sudo snap install eog
+
+
 # internet
-sudo snap install chromium whatsdesk
+sudo snap install viber-unofficial --edge
+sudo snap install chromium
+sudo snap install whatsdesk
 sudo snap install opera-developer --edge
+sudo snap install skype --classic
+sudo snap install instagraph
+sudo snap install tor
+
+
+# Amazon AWS cli
+# sudo snap install aws-cli --classic
+
+#dev
+# sudo snap install eclipse --classic
+# sudo snap install node --channel=10/stable --classic
+
+sudo snap install notepad-plus-plus
+sudo snap install notepadqq
+
+# sudo snap install sweethome3d-homedesign
